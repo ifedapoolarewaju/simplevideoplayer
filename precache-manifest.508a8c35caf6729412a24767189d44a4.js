65300d0e72288d085e1e3768eca9798f0dab3140
ifedapoolarewaju <ifedapoolarewaju@gmail.com>
2020-07-12 13:25:10 +0100

    Updates

---
+++ precache-manifest.508a8c35caf6729412a24767189d44a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd39591e5ba1bfc3ccddbc3da81e6bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "926fd09abbded9963190",
+    "url": "/static/css/main.f9555b76.chunk.css"
+  },
+  {
+    "revision": "c5728ff7b8dfeede85fc",
+    "url": "/static/js/2.cd650809.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd650809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "926fd09abbded9963190",
+    "url": "/static/js/main.a50b58c1.chunk.js"
+  },
+  {
+    "revision": "b80208fd14d418c025b5",
+    "url": "/static/js/runtime-main.c5bc6d3f.js"
+  },
+  {
+    "revision": "791150b50c03970d1007b32351d96f61",
+    "url": "/static/media/playicon.791150b5.png"
+  }
+]);
